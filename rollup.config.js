8700bd623b727163c8f048d27f208279d16541ee
Angel Calderaro <calderaro94@gmail.com>
2021-07-14 21:17:12 -0500

    update build

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,12 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import cleaner from 'rollup-plugin-cleaner';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import autoprefixer from 'autoprefixer';
 import packageJson from './package.json';
 
 export default {
-  input: 'src/index.ts',
+  input: './src/index.tsx',
   output: [
     {
       file: packageJson.main,
@@ -28,6 +30,10 @@ export default {
     commonjs(),
     typescript({
       exclude: ['**/*.stories.tsx', '**/*.test.tsx']
+    }),
+    postcss({
+      modules: true,
+      plugins: [autoprefixer()]
     })
   ]
 };
